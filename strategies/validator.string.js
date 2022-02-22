6bf489bdddbef98e8f6aa7edc8f2190e611399ae
Joaquin Eduardo Arreguez <joaquin.arreguez@naranjax.com>
2022-02-21 21:29:42 -0300

    feat(jlidate): add new options to string validation

--- strategies/validator.string.js
+++ strategies/validator.string.js
@@ -3,6 +3,35 @@
 function apply({ key, value }, conditions = {}) {
   const errors = [];
   if (typeof value === conditions.type) {
+    // regex pattern
+    if (conditions.pattern && !conditions.pattern.test(value)) {
+      errors.push(
+        `property ${key}(${value}) does not match with pattern (${conditions.pattern.toString()})`
+      );
+    }
+
+    // minLength
+    // TODO si minLength = 0 la validación del if dará false
+    if (
+      conditions.minLength &&
+      !isNaN(conditions.minLength) &&
+      value.length < conditions.minLength
+    ) {
+      errors.push(
+        `${key}(${value}) string length should be less than ${conditions.minLength}`
+      );
+    }
+
+    //maxLength
+    if (
+      conditions.maxLength &&
+      !isNaN(conditions.maxLength) &&
+      value.length > conditions.maxLength
+    ) {
+      errors.push(
+        `${key}(${value}) string length should be more than ${conditions.maxLength}`
+      );
+    }
   } else {
     errors.push(`property ${key}(${value}) is not ${conditions.type}`);
   }
--- test/validator.string.test.js
+++ test/validator.string.test.js
@@ -29,4 +29,127 @@ describe("Jlidate string validator", () => {
       [`property someString(${someString}) is not string`],
     ]);
   });
+
+  test("should fail if string.length < minLength", () => {
+    const someString = "has13letters";
+    const minLength = 30;
+    const validator = new Jlidate({
+      type: "object",
+      properties: {
+        someString: { type: "string", minLength },
+      },
+    });
+    const validationResult = validator.validate({ someString });
+    expect(validationResult).toEqual(false);
+    expect(validator.getErrors()).toEqual([
+      "",
+      [
+        `someString(${someString}) string length should be less than ${minLength}`,
+      ],
+    ]);
+  });
+
+  test("should fail if string.length > maxLength", () => {
+    const someString = "has13letters";
+    const maxLength = 3;
+    const validator = new Jlidate({
+      type: "object",
+      properties: {
+        someString: { type: "string", maxLength },
+      },
+    });
+    const validationResult = validator.validate({ someString });
+    expect(validationResult).toEqual(false);
+    expect(validator.getErrors()).toEqual([
+      "",
+      [
+        `someString(${someString}) string length should be more than ${maxLength}`,
+      ],
+    ]);
+  });
+
+  test("should pass if string is between range", () => {
+    const someString = "has13letters";
+    const maxLength = 14;
+    const minLength = 0;
+    const validator = new Jlidate({
+      type: "object",
+      properties: {
+        someString: { type: "string", maxLength, minLength },
+      },
+    });
+    const validationResult = validator.validate({ someString });
+    expect(validationResult).toEqual(true);
+  });
+
+  test("should pass if string complains to pattern", () => {
+    const someString = "hello joaquin";
+    const validator = new Jlidate({
+      type: "object",
+      properties: {
+        someString: { type: "string", pattern: /hello.*/ },
+      },
+    });
+    const validationResult = validator.validate({ someString });
+    expect(validationResult).toEqual(true);
+  });
+
+  test("should fail if string does not complain to pattern", () => {
+    const someString = "bye joaquin";
+    const pattern = /hello.*/;
+    const validator = new Jlidate({
+      type: "object",
+      properties: {
+        someString: { type: "string", pattern },
+      },
+    });
+    const validationResult = validator.validate({ someString });
+    expect(validationResult).toEqual(false);
+    expect(validator.getErrors()).toEqual([
+      "",
+      [
+        `property someString(${someString}) does not match with pattern (/hello.*/)`,
+      ],
+    ]);
+  });
+
+  test("should pass if string complains to all conditions", () => {
+    const someString = "hello joaquin";
+    const validator = new Jlidate({
+      type: "object",
+      properties: {
+        someString: {
+          type: "string",
+          pattern: /hello.*/,
+          minLength: 0,
+          maxLength: 13,
+        },
+      },
+    });
+    const validationResult = validator.validate({ someString });
+    expect(validationResult).toEqual(true);
+  });
+
+  test("should fail if string does not complain to any condition", () => {
+    const someString = "bye joaquin";
+    const minLength = 20;
+    const maxLength = 10;
+    const pattern = /hello.*/;
+    const validator = new Jlidate({
+      type: "object",
+      properties: {
+        someString: { type: "string", pattern, minLength, maxLength },
+      },
+    });
+    const validationResult = validator.validate({ someString });
+    expect(validationResult).toEqual(false);
+    expect(validator.getErrors()).toEqual([
+      "",
+      [
+        `property someString(${someString}) does not match with pattern (${pattern})`,
+        `someString(${someString}) string length should be less than ${minLength}`,
+        `someString(${someString}) string length should be more than ${maxLength}`,
+      ],
+    ]);
+  });
 });
